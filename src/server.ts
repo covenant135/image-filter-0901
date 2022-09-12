ae208743fdba7515a7408aeeb263483fad5d5f2c
SCHULL TECHNOLOGIES <abiodun.olaitan46@gmail.com>
2022-09-12 12:08:46 +0100

    in progress

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -31,6 +31,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
   
+app.get{"/filteredimage", async (req: @Reqeust,res: @Response) => {
+  const image_url =req.query.image_url.toString();
+  if(!image_url){
+    res.status(400).send('Image url is required');
+  }
+  const filetered_image =await filterImageFromURL(image_url);
+
+  response.status(200).sendFile(filetered_image,() => {
+    deleteLocalFiles({filtered_image});
+  });
+
+}};
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
